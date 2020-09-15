356603d2a71f36842eb69a8cb0d19f4e94cca8c1
zavadskiy <andreyzavadskiy@list.ru>
2020-09-15 20:57:52 +0300

    Fix github

---
+++ SP.Core/Log/ActionLog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SP.Core.Model;
+
+namespace SP.Core.History
+{
+    [Table("Action", Schema = "log")]
+    public class ActionLog
+    {
+        /// <summary>
+        /// Идентификатор записи
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Идентификатор персоны
+        /// </summary>
+        public int PersonId { get; set; }
+        /// <summary>
+        /// Дата действия
+        /// </summary>
+        public DateTime ActionDate { get; set; }
+        /// <summary>
+        /// Категория действий
+        /// </summary>
+        [StringLength(50)]
+        public string Category { get; set; }
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Description { get; set; }
+
+        #region Navigation properties
+
+        [ForeignKey("PersonId")]
+        public Person Person { get; set; }
+
+        #endregion
+    }
+}
