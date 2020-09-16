3ec8bbb9f39cf25bb7d27db54528bc0ba59d4b26
zavadskiy <andreyzavadskiy@list.ru>
2020-09-16 07:25:11 +0300

    Change report

---
+++ SP.Service/Models/ChangeListItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SP.Service.Models
+{
+    public class ActionListItem
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime ActionDate { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ SP.Service/Models/OrderDetailReportListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SP.Service.Models
+{
+    public class CalcSheetReportListItem
+    {
+        public string NomenclatureGroupName { get; set; }
+        public string NomenclatureName { get; set; }
+        public string StationNumber { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Plan { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
