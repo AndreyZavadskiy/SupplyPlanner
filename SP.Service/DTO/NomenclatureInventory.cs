2d11fd494354b2671afcbf3d62a3381f5f62f8db
zavadskiy <andreyzavadskiy@list.ru>
2020-09-17 23:12:54 +0300

    Missed files in Git

---
+++ SP.Service/DTO/NomenclatureInventory.cs
@@ -0,0 +1,9 @@
+﻿namespace SP.Service.DTO
+{
+    public class NomenclatureInventory
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string StationNumber { get; set; }
+    }
+}
