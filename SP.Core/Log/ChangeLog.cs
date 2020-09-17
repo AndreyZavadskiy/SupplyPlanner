b37db4c9defa83f6c771dd5098b0b07517b212ae
zavadskiy <andreyzavadskiy@list.ru>
2020-09-17 23:07:06 +0300

    Missed files

---
+++ SP.Core/Log/ChangeLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using SP.Core.Model;
+
+namespace SP.Core.Log
+{
+    [Table("Change", Schema = "log")]
+    public class ChangeLog
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public DateTime ChangeDate { get; set; }
+        [StringLength(50)]
+        public string EntityName { get; set; }
+        public int RecordId { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+
+        #region Navigation properties
+
+        [ForeignKey("PersonId")]
+        public Person Person { get; set; }
+
+        #endregion
+    }
+}
---
+++ SP.Service/Models/CalcSheetReportListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SP.Service.Models
+{
+    public class CalcSheetReportListItem
+    {
+        public string NomenclatureGroupName { get; set; }
+        public string NomenclatureName { get; set; }
+        public string StationNumber { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Plan { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
