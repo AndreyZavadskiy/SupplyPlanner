ac242f56dc2f5c35bfb43d18a926e670b2e6dabb
zavadskiy <andreyzavadskiy@list.ru>
2020-11-13 10:06:51 +0300

    CalcSheet history

---
+++ SP.Database/dbo/Triggers/CalcSheet_History.sql
@@ -0,0 +1,24 @@
+﻿CREATE TRIGGER [CalcSheet_History]
+ON [dbo].[CalcSheet]
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON
+    IF (ROWCOUNT_BIG() = 0)
+    RETURN;
+    INSERT INTO dbo.CalcSheetHistory (
+        [RecordId],
+        [EffectiveDate],
+        [NomenclatureId],
+        [GasStationId],
+        [Quantity],
+        [FixedAmount],
+        [Formula],
+        [MultipleFactor],
+        [Rounding],
+        [Plan],
+        [LastUpdate]
+        )
+    SELECT Id, LastUpdate, NomenclatureId, GasStationId, Quantity, FixedAmount, Formula, MultipleFactor, Rounding, [Plan], LastUpdate
+    FROM inserted;
+END
