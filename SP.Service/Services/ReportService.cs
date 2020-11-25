7ee34dff4637ecaf5eb3dfc6ef75209d015653c7
zavadskiy <andreyzavadskiy@list.ru>
2020-11-25 08:46:31 +0300

    StageInventory counter

--- SP.Service/Services/ReportService.cs
+++ SP.Service/Services/ReportService.cs
@@ -44,22 +44,21 @@ namespace SP.Service.Services
                 FROM ""Inventory"" i 
                 UNION
                 SELECT 4 AS id,
+                    'Загружено остатков' as ""IndicatorName"",
+                    to_char(COUNT(*), '999 999 999 999') AS ""IndicatorValue""
+                FROM public.""StageInventory""
+                UNION
+                SELECT 5 AS id,
                     'Последняя плановая поставка' as ""IndicatorName"",
                     to_char(max(o.""OrderDate""), 'DD.MM.YYYY HH24:MI') AS ""IndicatorValue""
                 FROM ""Order"" o
                 WHERE o.""OrderType"" = 2
                 UNION
-                SELECT 5 AS id,
+                SELECT 6 AS id,
                     'Последний заказ ТМЦ' as ""IndicatorName"",
                     to_char(max(o.""OrderDate""), 'DD.MM.YYYY HH24:MI') AS ""IndicatorValue""
                 FROM ""Order"" o
                 WHERE o.""OrderType"" = 1
-                UNION
-                SELECT 6 AS id,
-                    'Всего записей в БД' as ""IndicatorName"",
-                    to_char(SUM(n_live_tup), '999 999 999 999') AS ""IndicatorValue""
-                FROM pg_stat_user_tables 
-                WHERE schemaname != 'log'
                 ORDER BY id;";
 
             var indicators = await _context.Set<IndicatorView>()
