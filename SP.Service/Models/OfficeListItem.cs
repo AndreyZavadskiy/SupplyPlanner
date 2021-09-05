e793cb960a63b9b6af7ea801767e7153744d9810
zavadskiy <andreyzavadskiy@list.ru>
2021-09-06 02:15:07 +0300

    Offices

---
+++ SP.Service/Models/OfficeListItem.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SP.Service.Models
+{
+    /// <summary>
+    /// Нефтебаза
+    /// </summary>
+    public class OfficeListItem
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Название объекта сети
+        /// </summary>
+        public string ObjectName { get; set; }
+        /// <summary>
+        /// Количество персонала
+        /// </summary>
+        public int? PersonnelTotal { get; set; }
+        /// <summary>
+        /// Количество отделов
+        /// </summary>
+        public int? DepartmentTotal { get; set; }
+        /// <summary>
+        /// Количество флагштоков
+        /// </summary>
+        public int? FlagpoleTotal { get; set; }
+        /// <summary>
+        /// Централизованное водоснабжение
+        /// </summary>
+        public bool HasCentralWaterSupply { get; set; }
+    }
+}
---
+++ SP.Service/Models/OfficeModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SP.Service.Models
+{
+    /// <summary>
+    /// Нефтебаза
+    /// </summary>
+    public class OfficeModel
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        [Required(ErrorMessage = "Поле является обязательным")]
+        public int Id { get; set; }
+        /// <summary>
+        /// Название объекта сети
+        /// </summary>
+        [Required(ErrorMessage = "Поле является обязательным")]
+        [DisplayName("Название")]
+        public string ObjectName { get; set; }
+        /// <summary>
+        /// Адрес
+        /// </summary>
+        [DisplayName("Адрес")]
+        public string Address { get; set; }
+        /// <summary>
+        /// Количество персонала
+        /// </summary>
+        [DisplayName("Количество персонала")]
+        public int? PersonnelTotal { get; set; }
+        /// <summary>
+        /// Количество отделов
+        /// </summary>
+        [DisplayName("Количество отделов")]
+        public int? DepartmentTotal { get; set; }
+        /// <summary>
+        /// Количество флагштоков
+        /// </summary>
+        [DisplayName("Количество флагштоков")]
+        public int? FlagpoleTotal { get; set; }
+        /// <summary>
+        /// Централизованное водоснабжение
+        /// </summary>
+        [DisplayName("Централизованное водоснабжение")]
+        public bool HasCentralWaterSupply { get; set; }
+    }
+}
